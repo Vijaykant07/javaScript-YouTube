65c14efaabf8957ffa1ade1a5dde3495c8fb6cbf
Vijaykant07 <kumarvijaybp2@gmail.com>
2025-08-21 12:53:01 +0000

    01_variable config done

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,6 @@
+const accountId=123345
+let accountEmail="asd@123"
+var accountPassword="123"
+
+console.log(accountId,accountEmail,accountPassword)
+console.table([accountId,accountEmail,accountPassword])
