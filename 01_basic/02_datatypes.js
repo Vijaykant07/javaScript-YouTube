37b956fa32b3faad202a7a590ce27eedbc3272fa
Vijaykant07 <kumarvijaybp2@gmail.com>
2025-08-21 13:24:10 +0000

    datatype config done

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict";  //treat all js code as newer version
+
+//alert(3+3)  //we are using node js, not browser
+ 
+// console.log(3+3)
+//Primitive datatypes 
+//number 2^53
+///bigint    for large numbers
+// string " "  
+//boolean 
+//null -> stand alone value
+//undefined
+
+//object
+// console.log(typeof "Vijay");
+//let age = 21;
+//console.log(typeof age);
+
+let age = 21;
+console.log(typeof age);
+
+
+console.log(typeof null);  //object
+console.log(typeof undefined);  //undefined
