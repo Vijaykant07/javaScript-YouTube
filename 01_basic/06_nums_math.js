0d353663b652f2e2c215d63dc28525004c4ab458
Vijaykant07 <kumarvijaybp2@gmail.com>
2025-08-25 15:07:49 +0000

    nums_math

---
+++ 01_basic/06_nums_math.js
@@ -0,0 +1,28 @@
+const score=400
+//console.log(score);
+
+const balance=new Number(100)
+//console.log(balance);
+
+//console.log(balance.toString().length); //length
+//console.log(balance.toFixed(2));  //for how much deciml point need
+ 
+const otherNumber=123.4555
+//console.log(otherNumber.toPrecision(3)); //priotrity to before decimal no.
+
+
+const hundreds=100000
+//console.log(hundreds.toLocaleString('en-IN'));
+
+//+++++++++++++++++++++++ Maths ++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-3));
+// console.log(Math.round(4.5)); //round off
+// console.log(Math.ceil(4.5)); //upper value
+// console.log(Math.floor(4.5));  //lower value
+// console.log(Math.min(4,5,7,1));  // min value from arrays
+// console.log(Math.max(4,5,7,1));
+
+console.log(Math.random());
+console.log(Math.random()*10+1);
