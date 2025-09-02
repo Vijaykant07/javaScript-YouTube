9a809aaf4ffdecc464e27f388b5cabbbfee42f72
Vijaykant07 <kumarvijaybp2@gmail.com>
2025-09-02 10:01:07 +0000

    update fun

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -35,4 +35,32 @@ function loginUserMessage(username="vijay"){
 }
 
 //console.log(loginUserMessage("vijay"));
-console.log(loginUserMessage());
+//console.log(loginUserMessage());
+
+//function calculateCartPrice(...num1){   // ... means for array form
+function calculateCartPrice(val1,val2, ...num1){
+    return num1
+}
+//console.log(calculateCartPrice(200,400,500,1000));
+
+const user={
+    username:"vijay",
+    price:199
+}
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+    
+}
+//handleObject(user)
+handleObject({
+    username:"vijay",
+    price:288
+})
+
+const myNewArray=[200,299,300,400]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+//console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200,300,400,500]));
