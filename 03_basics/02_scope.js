20c9d35da6fef150a1c6aa406a30fa107e1bd086
Vijaykant07 <kumarvijaybp2@gmail.com>
2025-09-04 10:19:25 +0000

    scope2

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -5,13 +5,54 @@ let a=300
 if(true){
    let a=10
    const b=20
-    console.log("INNER: ",a);
+   // console.log("INNER: ",a);
     
 }
 
 
-console.log(a);
+//console.log(a);
 //console.log(b);
 //console.log(c);
 
 
+function one(){
+    const username="vijay"
+
+    function two(){
+        const website="youtube"
+        console.log(username);
+        
+    }
+    //console.log(website);
+    
+    two()
+    
+}
+//one()
+
+if(true){
+    const username="vijay"
+    if(username==="vijay"){
+        const website=" youtube"
+       // console.log(username+website);
+        
+    }
+   // console.log(website);
+    
+}
+//console.log(username);
+
+
+// +++++++++++++++++++++++ intersecting++++++++++++++++
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+addTwo(5)
+const addTwo=function(num){
+    return num+2
+}
