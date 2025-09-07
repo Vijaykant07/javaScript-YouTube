6aed3b2f3bf773fde275894f388f1c3d31fdd7f3
Vijaykant07 <kumarvijaybp2@gmail.com>
2025-09-07 13:25:28 +0000

    control Flow

---
+++ 04_controlFlow/switch.js
@@ -0,0 +1,34 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month="march"
+switch (month) {
+    case 1:
+        console.log("January");
+         break;
+     case 2:
+        console.log("Feb");
+         break;
+         
+          case 3:
+        console.log("march");
+         break;
+          case 4:
+        console.log("april");
+         break;
+          case 5:
+        console.log("may");
+         break;
+
+
+    default:
+        console.log("default case match");
+        
+        break;
+}
