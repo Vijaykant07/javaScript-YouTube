eb58f3955ff339188a77b472e543a3a7c930db76
Vijaykant07 <kumarvijaybp2@gmail.com>
2025-09-27 10:37:43 +0000

    switch_color

--- change_color_pro/changecolor.js
+++ change_color_pro/changecolor.js
@@ -1,19 +1,13 @@
+// Select all buttons
+const buttons = document.querySelectorAll('.button');
+// Select body
+const body = document.querySelector('body');
+
+// Add event listeners
 buttons.forEach(function (button) {
-  console.log(button);
   button.addEventListener('click', function (e) {
-    console.log(e);
-    console.log(e.target);
-    if (e.target.id === 'grey') {
-      body.style.backgroundColor = e.target.id;
-    }
-    if (e.target.id === 'white') {
-      body.style.backgroundColor = e.target.id;
-    }
-    if (e.target.id === 'blue') {
-      body.style.backgroundColor = e.target.id;
-    }
-    if (e.target.id === 'yellow') {
-      body.style.backgroundColor = e.target.id;
-    }
+    const color = e.target.id;  // get button id
+    console.log("Color selected:", color); // debug log
+    body.style.backgroundColor = color; // apply background
   });
 });
